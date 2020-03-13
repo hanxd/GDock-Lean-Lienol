397765178c45a186c52e1bab225c75b8904b1f57
hxd <X.HanXD@Gmail.com>
2020-03-14 05:28:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i 's/Openwrt/Kyara/g' package/base-files/files/bin/config_generate
 sed -i 's/Openwrt/Kyara/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
